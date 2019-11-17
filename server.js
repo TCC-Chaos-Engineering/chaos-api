4d51c2064f5a0fd2e2ae7ef917ff9bc99abd0398
momanhaes <momanhaes@gmail.com>
2019-11-17 13:07:33 -0300

    Sobe tutorial pra subir o servidor

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
@@ -9,11 +9,38 @@ app.use(express.json());
 app.use(cors());
 
 //Iniciando o BD
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
 
-requireDir('./src/models/');
+requireDir("./src/models/");
 
 // Rotas
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
+
+// TUTORIAL PARA SUBIR O SERVIDOR
+
+//Verificar se o docker está instalado
+// docker
+
+// Baixar o container do mongo
+// docker pull mongo
+
+// Subir um container com a instalação do mongo
+// Parâmetros: nome, redirecionamento de porta, imagem
+// docker run --name mongodb -p 27017:27017 -d mongo
+// Obs: Deve retornar um código.
+
+// Verificar quais imagens estão rodando
+// docker ps
+
+// Verificar quais imagens estão disponíveis
+// docker ps -a
+
+// Rodar a imagem do mongo
+// docker start mongo
+
+// Ir até ao navegador
+// localhost:27017
